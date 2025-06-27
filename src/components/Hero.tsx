1572ff3a543d537deb5f610ef55ef1ebe7167462
INDRIYANTO NUGROHO <masyantoya@gmail.com>
2025-06-27 22:11:41 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC = () => {
                 <p className="mt-4 text-foreground max-w-lg mx-auto">{heroDetails.subheading}</p>
                 <div className="mt-6 flex flex-col sm:flex-row items-center sm:gap-4 w-fit mx-auto">
                     <AppStoreButton dark />
-                    <PlayStoreButton dark />
+                    {/* <PlayStoreButton dark /> */}
                 </div>
                 <Image
                     src={heroDetails.centerImageSrc}
